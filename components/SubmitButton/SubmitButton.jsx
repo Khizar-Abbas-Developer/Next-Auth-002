01a6dfeb73896d26150125856545e80be964afe3
Mr. Khizar Abbas <khizarnot7@gmail.com>
2024-01-21 13:03:38 +0500

    lets go

--- components/SubmitButton/SubmitButton.jsx
+++ components/SubmitButton/SubmitButton.jsx
@@ -1,12 +1,20 @@
-
 "use client";
-import {useFormStatus} from "react-dom";
+import { useFormStatus } from "react-dom";
+import { ScaleLoader } from "react-spinners";
 
-const SignUpButton = ({name}) => {
+const SignUpButton = ({ name }) => {
     const { pending } = useFormStatus();
     return (
         <button className="w-full max-w-[150px] m-auto bg-red-500 hover:bg-red-600 cursor-pointer text-white text-xl font-medium text-center py-1 rounded-full mt-4">
-            {pending ? "please wait..." : `${name}`}
+            {pending ? (
+                <>
+                    <ScaleLoader color="#FFFF00" height={20} width={4} />
+                </>
+            ) : (
+                <>
+                    {name}
+                </>
+            )}
         </button>
     );
 };
