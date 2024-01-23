0957531c5850e8e853b74df02a1d2f5505125167
Mr. Khizar Abbas <khizarnot7@gmail.com>
2024-01-23 16:18:20 +0500

    adsense

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4815404807688326"
+        crossorigin="anonymous"></script>
+      </head>
       <body className={inter.className}>
         <NextTopLoader color='red' showSpinner={false} />
         <Toaster />
