4d5b8d980881ce90bdce8f42e068d4cd1affa8ca
Mr. Khizar Abbas <khizarnot7@gmail.com>
2024-01-27 16:17:11 +0500

    improved-004

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -104,7 +104,7 @@ const Navbar = () => {
                 </div>
                 </div>
                 <div className="flex-1">
-                    <Link href={"/"} className="">
+                    <Link href={"/"} className="" aria-label="home-page">
                         <Image src={logo} width={40} height={40} className="h-auto w-auto" alt="" priority />
                     </Link>
                 </div>
