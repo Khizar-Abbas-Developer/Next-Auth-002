162801145ca51f1bbbd893286343a8b75c93f185
Mr. Khizar Abbas <khizarnot7@gmail.com>
2024-02-08 02:50:32 +0500

    publish

--- app/(Auth)/edit-profile/page.js
+++ app/(Auth)/edit-profile/page.js
@@ -1,15 +1,19 @@
-"use client"
 import Profile from "@/components/Profile/Profile";
-const metadata = {
+let userData;
+const handleProfileData = async (data) => {
+  userData = await data
+  const username = data.username;
+  const profile = data.image;
+};
+export const metadata = {
   openGraph: {
-    title: 'Next.js',
+    title: {username},
     description: 'The React Framework for the Web',
     url: 'https://next-auth-002.vercel.app',
     siteName: 'Next.js Authentication',
     images: [
       {
-        url: 'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500', // Must be an absolute URL
-        width: 800,
+        url: `${profile}`,
         height: 600,
       }
     ],
@@ -18,9 +22,6 @@ const metadata = {
   },
 }
 export default function UserProfileEdit() {
-  const handleProfileData = (data) => {
-    console.log(data);
-  };
   return (
     <>
       <Profile onProfileData={handleProfileData} />
